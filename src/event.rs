acab7c543091a29f895f4c670591240fdd9c8273
WeirdPtr <weirdptr@weirdptr.com>
2023-07-15 11:56:32 +0200

    chore(event): cleanup api

--- src/event.rs
+++ src/event.rs
@@ -1,33 +1,19 @@
-#[cfg(feature = "uuid")]
-use uuid::Uuid;
-
 pub struct Event<T> {
-    #[cfg(feature = "uuid")]
-    uuid: String,
     pub data: T,
 }
 
 impl<T> Event<T> {
-    pub fn new(data: T) -> Event<T> {
-        Event {
-            #[cfg(feature = "uuid")]
-            uuid: Uuid::new_v4().to_string(),
-            data,
-        }
+    pub const fn new(data: T) -> Self {
+        Self { data }
     }
 
-    pub fn get_data(&self) -> &T {
-        return &self.data;
-    }
-
-    #[cfg(feature = "uuid")]
-    pub fn get_uuid(&self) -> String {
-        return self.uuid.clone();
+    pub const fn data_ref(&self) -> &T {
+        &self.data
     }
 }
 
 impl<T> From<T> for Event<T> {
     fn from(data: T) -> Self {
-        Event::new(data)
+        Self::new(data)
     }
 }
