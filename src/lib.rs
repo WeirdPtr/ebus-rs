f7defe06d2aaaf28cfc41324ee009f9f357f7737
WeirdPtr <weirdptr@weirdptr.com>
2023-07-15 11:56:05 +0200

    feat(structure): cleanup crate structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-pub mod bus;
-pub mod event;
-pub mod subscriber;
+#![allow(clippy::module_name_repetitions)]
+
+pub(crate) mod bus;
+pub(crate) mod event;
+
+pub type EventBus<T> = bus::EventBus<T>;
+pub type Event<T> = event::Event<T>;
+
+#[async_trait::async_trait]
+pub trait EventBusSubscriber: Send + Sync + 'static {
+    type InputDataType;
+
+    async fn on_event_publish(&mut self, event: &Event<Self::InputDataType>);
+}
