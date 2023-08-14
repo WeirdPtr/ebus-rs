8e99d273b856128b47260917ebedafce8d9101d7
WeirdPtr <weirdptr@weirdptr.com>
2023-08-14 17:33:30 +0200

    feat(subscriber): export crate used async_trait macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,15 @@
 pub(crate) mod bus;
 pub(crate) mod event;
 
+#[cfg(feature = "async_subscriber")]
 pub use async_trait::async_trait as async_subscriber;
 
 pub type EventBus<T> = bus::EventBus<T>;
 pub type Event<T> = event::Event<T>;
 
-#[async_subscriber]
+#[async_trait::async_trait]
 pub trait EventBusSubscriber: Send + Sync + 'static {
     type InputDataType;
-
+    
     async fn on_event_publish(&mut self, event: &Event<Self::InputDataType>);
 }
